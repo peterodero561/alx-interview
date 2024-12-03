b0970da1997cdf1008fcb6ade64da9f4980272aa
Peter Odero <peterodero561@gmail.com>
2024-12-03 07:51:25 +0300

    fix: considered index errors

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,18 +4,22 @@
 
 def island_perimeter(grid):
     '''returns perimeter of the island in the grid'''
+    # handle empty grid
+    if not grid or not grid[0]:
+        return 0
+
     count = 0  # will serve to count the distance of island perimeter
     width = len(grid)
+    length = len(grid[0])
     for i in range(width):
-        length = len(grid[i])
         for j in range(length):
             if (grid[i][j] == 1):
-                if (grid[i][j-1] == 0):
+                if (grid[i][j-1] == 0 or j == 0):
                     count += 1
-                if (grid[i-1][j] == 0):
+                if (grid[i-1][j] == 0 or i == 0):
                     count += 1
-                if (grid[i][j+1] == 0):
+                if (grid[i][j+1] == 0 or j == length-1):
                     count += 1
-                if (grid[i+1][j] == 0):
+                if (grid[i+1][j] == 0 or i == width-1):
                     count += 1
     return count
