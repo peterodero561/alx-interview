d3369256370fe0e5b0ecfe1c1e97c87c80107bd9
Peter Odero <peterodero561@gmail.com>
2024-12-12 23:18:49 +0300

    new look

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,53 +1,56 @@
 #!/usr/bin/python3
-'''Prime game code: determines if a given number
-is prime and plays with the number'''
-
-
-def is_prime(num):
-    '''Determines if the given num parameter is a prime number'''
-    if num == 1:
-        return False
-    elif num % 2 == 0 and num != 2:
-        return False
-    elif num % 3 == 0 and num != 3:
-        return False
-    else:
-        n = 5
-        while n < num:
-            if num % n == 0 and num != n:
-                return False
-            n += 2
-        return True
+'''Python prime game to determine a winner using given number'''
 
 
 def isWinner(x, nums):
-    '''Determins a winner among Maria and Ben
-    params:
-        x-number of rounds to play the game
-        nums-an array of numbers to choose prime number from
-    Return:
-        the winner btwn Maria and Ben
-    '''
-    if nums == []:
-        return None
-    if x == 0:
-        return None
+    '''method to choose who is winnner btwn Maria and Ben'''
+    def sieve(n):
+        """Generates a list of prime numbers up to n using
+        the Sieve of Eratosthenes."""
+        is_prime = [True] * (n + 1)
+        is_prime[0] = is_prime[1] = False
+        for i in range(2, int(n**0.5) + 1):
+            if is_prime[i]:
+                for j in range(i * i, n + 1, i):
+                    is_prime[j] = False
+        return is_prime
 
-    count = 0
-    for _ in range(x):
-        for i in nums:
-            if i == 0:
-                break
-            new_list = []
-            for j in range(1, i+1):
-                new_list.append(j)
-            for k in new_list:
-                if is_prime(k):
-                    count += 1
+    # Find the maximum n in nums to precompute primes
+    max_n = max(nums)
+    primes = sieve(max_n)
 
-    if count == 0:
-        return None
-    elif (count % 2 == 0):
-        return 'Ben'
+    def calculate_moves(n):
+        """Calculates the number of moves possible for a given n."""
+        numbers = list(range(1, n + 1))
+        used = [False] * (n + 1)
+        moves = 0
+
+        for i in range(2, n + 1):
+            if primes[i] and not used[i]:
+                moves += 1
+                # Mark i and its multiples as used
+                for j in range(i, n + 1, i):
+                    used[j] = True
+
+        return moves
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if n == 1:
+            ben_wins += 1  # No primes available, Ben wins
+        else:
+            moves = calculate_moves(n)
+            # Maria wins if the number of moves is odd, Ben wins if it's even
+            if moves % 2 == 1:
+                maria_wins += 1
+            else:
+                ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
     else:
-        return 'Maria'
+        return None
