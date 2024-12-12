e016ddb5b5f97b2be81cf98bc56dc3dd2165200c
Peter Odero <peterodero561@gmail.com>
2024-12-12 22:58:31 +0300

    created a function to determine a prime number and play a prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,14 +3,19 @@
 
 def is_prime(num):
     '''Determines if the given num parameter is a prime number'''
-
-
-def has_prime(nums):
-    '''Determines if the nums array parameter given has a Prime number'''
-    for num in nums:
-        if is_prime(num):
-            return True
-    return False
+    if num == 1:
+        return False
+    elif num % 2 == 0 and num != 2:
+        return False
+    elif num % 3 == 0 and num != 3:
+        return False
+    else:
+        n = 5
+        while n < num:
+            if num % n == 0 and num != n:
+                return False
+            n += 2
+        return True
 
 
 def isWinner(x, nums):
@@ -21,11 +26,26 @@ def isWinner(x, nums):
     Return:
         the winner btwn Maria and Ben
     '''
+    if nums == []:
+        return None
+    if x == 0:
+        return None
+
     count = 0
     for _ in range(x):
-        if has_prime(nums):
-            count += 1
-    if (count % 2 = 0):
-        return 'Winner: Ben'
+        for i in nums:
+            if i == 0:
+                break
+            new_list = []
+            for j in range(1, i+1):
+                new_list.append(j)
+            for k in new_list:
+                if is_prime(k):
+                    count += 1
+
+    if count == 0:
+        return None
+    elif (count % 2 == 0):
+        return 'Ben'
     else:
-        return 'Winner: Maria'
+        return 'Maria'
